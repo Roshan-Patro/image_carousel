db3bf8bbf5c1b382726d4166c18658a45bbbf2ec
T Roshan Kumar Patro <patro.roshan1997@gmail.com>
2022-10-30 23:37:47 +0530

    progress added

--- js files/index.js
+++ js files/index.js
@@ -1,3 +1,14 @@
 const productContainers = [...document.querySelectorAll('.newsCard-container')];
 const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
 const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width/2;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    
+})
